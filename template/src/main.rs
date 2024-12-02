2471fba6c0b4587fcef90a490e1b1a2efc0a31be
afaure42 <afaure@student.42.fr>
2024-12-03 00:51:30 +0100

    added template day

---
+++ template/src/main.rs
@@ -0,0 +1,20 @@
+
+use std::str::Lines;
+
+fn main() {
+	let input = include_str!("../input.txt");
+
+
+	println!("RET1: {}", solve1(input.lines()));
+	println!("RET2: {}", solve2(input.lines()));
+}
+
+fn solve1 (lines : Lines) -> i128
+{
+	return 0;
+}
+
+fn solve2 (lines : Lines) -> i128
+{
+	return 0;
+}
