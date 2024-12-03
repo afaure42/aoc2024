a6518ebfb53bce3bbc971750b027d9ecff853385
afaure42 <afaure@student.42.fr>
2024-12-03 20:16:24 +0100

    DAY02 finished

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,4 +1,6 @@
+// use core::num;
 use std::str::Lines;
+// use std::collections::VecDeque;
 
 fn main() {
 	let input = include_str!("../input.txt");
@@ -26,14 +28,10 @@ fn solve1 (lines : Lines) -> i128
 			numbers.push(n.parse::<i64>().expect("There should only be numbers in file"));
 		}
 
-		let mut incr = numbers.chunk_by(|a: &i64, b: &i64| a < b && b - a <= 3);
-		let mut decr = numbers.chunk_by(|a: &i64, b: &i64| a > b && a - b <= 3);
-
-		if decr.next().unwrap().len() == numbers.len()
-			|| incr.next().unwrap().len() == numbers.len() {
-				ret += 1;
-			}
-
+		if test_vec(&numbers) {
+			ret += 1;
+		}
+		
 	}
 	return ret;
 }
@@ -42,7 +40,7 @@ fn solve1 (lines : Lines) -> i128
 fn solve2 (lines : Lines) -> i128
 {
 	let mut ret = 0;
-	let mut line_nbr = 1;
+	// let mut line_nbr = 1;
 	for line in lines
 	{
 		let mut numbers: Vec<i64> = Vec::new();
@@ -51,60 +49,46 @@ fn solve2 (lines : Lines) -> i128
 			numbers.push(n.parse::<i64>().expect("There should only be numbers in file"));
 		}
 
-		let mut incr = numbers.chunk_by(|a: &i64, b: &i64| a < b && b - a <= 3).peekable();
-		let mut decr = numbers.chunk_by(|a: &i64, b: &i64| a > b && a - b <= 3).peekable();
-
-		if decr.peek().unwrap().len() == numbers.len()
-		|| incr.peek().unwrap().len() == numbers.len() {
+		if test_vec(& numbers) {
 			ret += 1;
+			continue;
 		}
-		else {
-			let incr_vec: Vec<&[i64]> = incr.collect();
-			let decr_vec: Vec<&[i64]> = decr.collect();
+		for n in 0..numbers.len()
+		{
+			let mut num_cpy = numbers.clone();
+			num_cpy.remove(n);
 
-			if incr_vec.len() == 2 {
-				println!("incr Line : {line_nbr}, {incr_vec:?}");
-			}
-			if decr_vec.len() == 2 {
-				println!("Decr Line : {line_nbr}, {decr_vec:?}");
-			}
-
-			if potential_valid(&incr_vec) {
+			if test_vec(& num_cpy) {
 				ret += 1;
-			} else if potential_valid(&decr_vec) {
-				ret += 1;
+				break;
 			}
 		}
-		line_nbr += 1;
+		// else {
+		// 	let incr_vec: Vec<&[i64]> = incr.collect();
+		// 	let decr_vec: Vec<&[i64]> = decr.collect();
+
+		// 	if incr_vec.len() == 2 {
+		// 		println!("incr Line : {line_nbr}, {incr_vec:?}");
+		// 	}
+		// 	if decr_vec.len() == 2 {
+		// 		println!("Decr Line : {line_nbr}, {decr_vec:?}");
+		// 	}
+
+		// 	if potential_valid(&incr_vec) {
+		// 		ret += 1;
+		// 	} else if potential_valid(&decr_vec) {
+		// 		ret += 1;
+		// 	}
+		// }
+		// line_nbr += 1;
 	}
 	return ret;
 }
 
-fn potential_valid (numbers: &Vec<&[i64]>) -> bool{
+fn test_vec (numbers: & Vec<i64>) -> bool {
+	let mut incr = numbers.chunk_by(|a: &i64, b: &i64| a < b && b - a <= 3).peekable();
+	let mut decr = numbers.chunk_by(|a: &i64, b: &i64| a > b && a - b <= 3).peekable();
 
-	if numbers.len() == 2 {
-		let first_len = numbers[0].len();
-		let second_len = numbers[1].len();
-
-		if first_len == 1 || second_len == 1 {
-			println!("Valid because first or last is wrong: {numbers:?}");
-			return true;
-		}
-		// now we now we have at least [a, b][c, d]
-		//test distance between a and c, b and d
-		// to check if b or c could be removed
-		let a: i64 = numbers[0][first_len - 2];
-		let b: i64 = numbers[0][first_len - 1];
-		let c: i64 = numbers[1][second_len - 2];
-		let d: i64 = numbers[1][second_len - 1];
-
-		if a.abs_diff(c) <= 3{
-			return true;
-
-		}
-		if b.abs_diff(d) <= 3{
-			return true;
-		}
-	}
-	return false;
+	return decr.peek().unwrap().len() == numbers.len()
+	|| incr.peek().unwrap().len() == numbers.len()
 }
