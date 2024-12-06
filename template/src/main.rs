4c9d1c7f636e83e1a0732b41805eb254f60faa92
afaure42 <afaure@student.42.fr>
2024-12-06 13:53:39 +0100

    modified template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,20 +1,18 @@
 
-use std::str::Lines;
-
 fn main() {
 	let input = include_str!("../input.txt");
 
 
-	println!("RET1: {}", solve1(input.lines()));
-	println!("RET2: {}", solve2(input.lines()));
+	println!("RET1: {}", solve1(input));
+	println!("RET2: {}", solve2(input));
 }
 
-fn solve1 (lines : Lines) -> i128
+fn solve1 (input : &str) -> i128
 {
 	return 0;
 }
 
-fn solve2 (lines : Lines) -> i128
+fn solve2 (lines : &str) -> i128
 {
 	return 0;
 }
