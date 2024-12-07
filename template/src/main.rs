19f82cb0e65478169d1359ce91e388e7ade53763
afaure42 <afaure@student.42.fr>
2024-12-07 12:44:44 +0100

    modified template to add modules

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,18 +1,10 @@
+mod solve1;
+mod solve2;
 
 fn main() {
 	let input = include_str!("../input.txt");
 
 
-	println!("RET1: {}", solve1(input));
-	println!("RET2: {}", solve2(input));
-}
-
-fn solve1 (input : &str) -> i128
-{
-	return 0;
-}
-
-fn solve2 (lines : &str) -> i128
-{
-	return 0;
+	println!("RET1: {}", solve1::solve(input));
+	println!("RET2: {}", solve2::solve(input));
 }
---
+++ template/src/solve1.rs
@@ -0,0 +1,4 @@
+
+pub fn solve(input: &str) -> i128 {
+	0
+}
