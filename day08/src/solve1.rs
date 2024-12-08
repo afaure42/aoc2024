8c237a670a70c4cb02fdd724fd4eea9fa759da46
afaure42 <afaure@student.42.fr>
2024-12-08 11:29:53 +0100

    DAY08 Working !

---
+++ day08/src/solve1.rs
@@ -0,0 +1,48 @@
+use std::{char, collections::{HashMap, HashSet}};
+use nalgebra::{Vector2};
+
+
+pub fn solve(input: &str) -> i128 {
+	let mut antennas: HashMap<char, Vec<Vector2<i32>>> = HashMap::new();
+	let mut y_max = 0;
+	let x_max= input.lines().next().unwrap().len() as i32;
+	let mut antinodes: HashSet<Vector2<i32>> = HashSet::new();
+
+	for (line_nbr, line) in input.lines().enumerate() {
+		for (index, c) in line.chars().enumerate() {
+			if c != '.' {
+				antennas
+					.entry(c)
+					.or_insert(Vec::new())
+					.push(Vector2::new(index as i32, line_nbr as i32));
+			}
+		}
+		y_max += 1;
+	}
+
+
+	//then do the math
+	for pos_vec in antennas.values() {
+		for pos_a in pos_vec {
+			for pos_b in pos_vec {
+				if pos_a == pos_b {continue}
+
+				let antinode_a: Vector2<i32> = pos_a + (pos_a - pos_b);
+				let antinode_b: Vector2<i32> = pos_b + (pos_b - pos_a);
+
+				if valid_pos(antinode_a, y_max, x_max) {antinodes.insert(antinode_a);}
+				if valid_pos(antinode_b, y_max, x_max) {antinodes.insert(antinode_b);}
+
+			}
+		}
+	}
+
+	antinodes.len() as i128
+}
+
+fn valid_pos (pos: Vector2<i32>, y_max: i32, x_max: i32) -> bool {
+	pos.x >= 0 
+	&& pos.x < x_max
+	&& pos.y < y_max
+	&& pos.y >= 0
+}
