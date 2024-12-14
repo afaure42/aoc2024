b0d194f73624199c561fe43c6b415e7c250ea4c6
afaure42 <afaure@student.42.fr>
2024-12-14 19:35:23 +0100

    more tests

--- src/utils/math/tests.rs
+++ src/utils/math/tests.rs
@@ -1,3 +1,5 @@
+use crate::utils::math::ParseVec2Error;
+
 use super::Vec2;
 
 #[test]
@@ -6,4 +8,16 @@ fn add() {
 	let b = Vec2::<i64>::new(20, 20);
 
 	assert_eq!(b, a + a)
+}
+
+#[test]
+fn parse_i32() {
+	let a = "30,30".parse::<Vec2<i32>>().unwrap();
+
+	assert_eq!(a, Vec2::<i32>::new(30, 30));
+}
+
+#[test]
+fn parse_error() {
+	assert_eq!("30,".parse::<Vec2<i32>>(), Err(ParseVec2Error));
 }
