efbdf31208c052be2e8458a863f80dffe884e680
afaure42 <afaure@student.42.fr>
2024-12-15 14:18:50 +0100

    modified template

--- template/main.rs
+++ template/main.rs
@@ -1,8 +1,17 @@
+use std::env;
 mod solve1;
 mod solve2;
 
 fn main() {
-	let input = include_str!("input.txt");
+	let args: Vec<_> = env::args().collect();
+	let input;
+
+	if args.len() > 1
+	&& args[1] == "test" {
+		input  = include_str!("input_bis.txt");
+	} else {
+		input = include_str!("input.txt");
+	}
 
 
 	println!("RET1: {}", solve1::solve(input));
